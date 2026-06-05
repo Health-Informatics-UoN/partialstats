8644d173c062d365582d662e342630cb0148b312
James Mitchell-White <jamesjimitchell@gmail.com>
2026-06-05 10:26:53 +0100

    function overloading

--- src/partialstats/aggregate_transforms/statistical.py
+++ src/partialstats/aggregate_transforms/statistical.py
@@ -1,10 +1,11 @@
 from math import sqrt
-from typing import Iterable
+from typing import Iterable, overload
 
 from ..partial_results.protocol import (
     CountPartialProtocol,
+    SumCountPartial,
     SumPartialProtocol,
-    MeanPartialProtocol,
+    SumSumSqCountPartialProtocol,
     VariancePartialProtocol,
 )
 from .core import sum_and_transform
@@ -20,19 +21,35 @@ def sum(partials: Iterable[SumPartialProtocol]) -> float:
     return sum_and_transform(lambda x: x.sum, partials)
 
 
-def mean(partials: Iterable[MeanPartialProtocol]) -> float:
+def mean(partials: Iterable[SumCountPartial]) -> float:
     """Combines partial results into a global mean."""
     return sum_and_transform(lambda x: x.sum / x.count, partials)
 
 
-def variance(partials: Iterable[VariancePartialProtocol]) -> float:
+@overload
+def variance(partials: Iterable[VariancePartialProtocol]) -> float: ...
+
+
+@overload
+def variance(partials: Iterable[SumSumSqCountPartialProtocol]) -> float: ...
+
+
+def variance(partials) -> float:
     """Combines partial results into a global population variance."""
-    return sum_and_transform(
-        lambda x: x.sum_of_squares / x.count - (x.sum / x.count) ** 2, partials
-    )
+    probe = list(partials)[0]
+    if (
+        hasattr(probe, "sum")
+        & hasattr(probe, "sum_of_squares")
+        & hasattr(probe, "count")
+    ):
+        return sum_and_transform(
+            lambda x: x.sum_of_squares / x.count - (x.sum / x.count) ** 2, partials
+        )
+    else:
+        return sum_and_transform(lambda x: x.variance, partials)
 
 
-def std(partials: Iterable[VariancePartialProtocol]) -> float:
+def std(partials: Iterable[SumSumSqCountPartialProtocol]) -> float:
     """Combines partial results into a global population standard deviation."""
     return sum_and_transform(
         lambda x: sqrt(x.sum_of_squares / x.count - (x.sum / x.count) ** 2), partials
